557511f056673e0c3ae814eae2665641fad063bd
Ion Olaru <ion_olaru@yahoo.com>
2026-02-23 21:07:47 +0100

    feat: complete Ex01 variables and printing

--- src/main/java/com/ionolaru/javafinprep/exercises/Ex01_VariablesAndPrinting.java
+++ src/main/java/com/ionolaru/javafinprep/exercises/Ex01_VariablesAndPrinting.java
@@ -2,6 +2,10 @@ package com.ionolaru.javafinprep.exercises;
 
 public class Ex01_VariablesAndPrinting {
     public static void main(String[] args) {
-        // TODO
+        String name = "Ion Olaru";
+        int age = 33;
+        double heightMeters = 1.8;
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + heightMeters + "m");
+        System.out.printf("Name: %s, Age: %d, Height: %.2fm%n", name, age, heightMeters);
     }
 }
